57e7589685ed357761b72eab884143b544c387db
songhwanOh <129318634+songhwanOh@users.noreply.github.com>
2023-08-08 17:05:22 -0400

    update data

--- data.js
+++ data.js
@@ -30,6 +30,36 @@ window.reviewData = [
       rating: 5,
       review: "Thumbs up!"
     },
+    {
+      name: "John smith",
+      date: "2023-08-04",
+      rating: 4,
+      review: "This game is amazing! I can't stop playing it."
+    },
+    {
+      name: "Jane Person",
+      date: "2023-08-04",
+      rating: 5,
+      review: "Best game ever! Highly recommended."
+    },
+    {
+      name: "Jasan Borne",
+      date: "2023-08-05",
+      rating: 2,
+      review: "You call this a game?"
+    },
+    {
+      name: "Tom NotHanks",
+      date: "2023-08-06",
+      rating: 5,
+      review: "Must play!"
+    },
+    {
+      name: "Sharon Smith",
+      date: "2023-08-07",
+      rating: 5,
+      review: "Thumbs up!"
+    },
     // Add more review objects here...
   ];
   
