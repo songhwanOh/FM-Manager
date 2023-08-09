8c4dd2be6aac77a61aefbc743ccac5e4defd89ff
songhwanOh <129318634+songhwanOh@users.noreply.github.com>
2023-08-09 17:17:00 -0400

    update comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
 
-    const main = document.querySelector("main");
-    const reviewData = window.reviewData;
+    const main = document.querySelector("main"); // Get a reference to the <main> element 
+    const reviewData = window.reviewData; // Get review data from the window object
   
+    // generate a review card HTML element
     function generateReviewCard(review) {
         const card = document.createElement("div");
         card.classList.add("review-card");
@@ -16,20 +17,21 @@ document.addEventListener("DOMContentLoaded", function () {
         return card;
       }
       
-      
+      // generate rating stars using Unicode characters
     function generateRatingStars(rating) {
       const fullStars = "★".repeat(rating);
       const emptyStars = "☆".repeat(5 - rating);
       return `${fullStars}${emptyStars}`;
     }
   
+    // display a review card
     function showReviewCard(review) {
         const newReviewContainer = document.getElementById("newReviewContainer");
         const reviewCard = generateReviewCard(review);
         newReviewContainer.insertAdjacentElement("beforebegin", reviewCard);
       }
       
-    
+    // remove the currently displayed review card
     function removeCurrentReviewCard() {
         const currentReviewCard = document.querySelector(".review-card");
         if (currentReviewCard) {
@@ -37,10 +39,12 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
     
+    // clear all review cards
     function clearReviewCards() {
         main.querySelectorAll(".review-card").forEach((card) => card.remove());
     }
   
+    // generate review links in the sidebar
     function generateReviewLinks() {
         const reviewLinksWrapper = document.getElementById("reviewLinks");
         const reviewsList = document.createElement("ul");
@@ -56,6 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
         reviewLinksWrapper.appendChild(reviewsList); // Append the updated review links
     }
       
+    // handle clicking on a review link
     function handleClick(event) {
         const reviewIndex = event.target.getAttribute("data-index");
         if (reviewIndex !== null) {
@@ -66,42 +71,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
       
-  
+  // show the first review card when the page loads
     function showFirstReviewCard() {
         const firstReview = reviewData[0];
         showReviewCard(firstReview);
         generateReviewLinks(); // Generate the review links after showing the first review
     }
       
-
+    // Generate initial review links and show the first review card
     generateReviewLinks();
     showFirstReviewCard();
   
+    // Event listener for clicks on the main content area
     main.addEventListener("click", handleClick);
-  
-    const reviewForm = document.getElementById("newReviewForm");
-  
-    reviewForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-        const name = document.getElementById("name").value;
-        const date = new Date().toISOString().slice(0, 10);
-        const rating = parseInt(document.getElementById("rating").value);
-        const review = document.getElementById("review").value;
-
-        const newReview = { name, date, rating, review };
-        reviewData.push(newReview);
-
-        clearReviewCards();
-        generateReviewLinks();
-    //   showFirstReviewCard();
-        showReviewCard(newReview)
-  
-      reviewForm.reset();
-    });
 
     const newReviewForm = document.getElementById("newReviewForm");
 
+    // Event listener for submitting the new review form
     newReviewForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
